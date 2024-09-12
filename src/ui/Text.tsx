6d3e7a3b865ba18bfa20705d87e7c094219d987b
proff00 <vladimir.ershov.omsk@gmail.com>
2024-09-12 12:27:56 +0300

    create text

---
+++ src/ui/Text.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import secondary from './common/global.scss'
+
+const Text = () => (
+    <div className="text">
+        <p color="secondary">
+        </p>
+
+    </div>
+)
+export default Text;
