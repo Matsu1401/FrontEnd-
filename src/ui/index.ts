4fcf53f58f5b2133975c749ecd43290ae99df767
proff00 <vladimir.ershov.omsk@gmail.com>
2024-11-28 16:37:36 +0300

    Create IconButtonComponent

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -4,3 +4,4 @@ export * as Utils from './Utils'
 
 export { default as Text } from './components/Text/Text'
 export { default as Icon } from './components/Icon/Icon'
+export { default as IconButton } from './components/Iconbutton/Iconbutton'
